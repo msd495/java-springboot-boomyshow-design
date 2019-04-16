6b6988f42b75bbf8c207262fc7030a70ef1303dc
Ayush495 <msd495@gmail.com>
2019-04-16 15:58:41 +0530

    Create Mail.java

---
+++ src/main/java/bookmyshow/model/Mail.java
@@ -0,0 +1,36 @@
+package bookmyshow.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Mail {
+
+    private String msgTo;
+
+    public String getMsgTo() {
+        return msgTo;
+    }
+
+    public void setMsgTo(String msgTo) {
+        this.msgTo = msgTo;
+    }
+
+    public String getMsgBody() {
+        return msgBody;
+    }
+
+    public void setMsgBody(String msgBody) {
+        this.msgBody = msgBody;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    private String msgBody;
+    private String subject;
+}
